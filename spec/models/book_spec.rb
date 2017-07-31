23a3b9a9b96a733e7ed54182ea4bdf2608330f72
Michelle Nosal <nosalcontact@yahoo.com>
2017-07-31 15:34:12 -0400

    create books model and tests

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  it { should have_valid(:title).when("The Big Book of Books") }
+  it { should_not have_valid(:title).when('', nil) }
+
+  it { should have_valid(:author).when("Jane Doe") }
+  it { should_not have_valid(:author).when('', nil) }
+end
