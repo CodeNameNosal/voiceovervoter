a730526b9b5c5a938fe4ed8a9b05401bce7c2b4d
Glenn R. Martin <grmartin@programmer.net>
2017-08-01 15:21:28 -0400

    Changes to include object.

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -1,18 +1,7 @@
 class Api::V1::MatchesController < ApplicationController
   def index
-    @relevantMatches = Match.where({ book_id: params[:book_id] })
+    @relevantMatches = Match.where(book_id: params[:book_id])
 
-    render json: { relevantMatches: @relevantMatches }
+    render json: { relevantMatches: @relevantMatches }, include: [:voice]
   end
 end
-
-
-
-
-# class Match < ActiveRecord::Base
-#   belongs_to :voices, :inverse_of => :match
-# end
-#
-# class Voice < ActiveRecord::Base
-#   has_many :match, :inverse_of => :voices
-# end
