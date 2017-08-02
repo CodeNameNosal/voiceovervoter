d1cdb0e327a6d074014f98db8aa910d80f507eef
Michelle Nosal <nosalcontact@yahoo.com>
2017-08-02 16:50:26 -0400

    git errors

---
+++ app/controllers/api/v1/matched_voices_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::MatchedVoicesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @relevantMatches = MatchedVoice.where({ book_id: params[:book_id] })
+
+    render json: { relevantMatches: @relevantMatches }
+  end
+
+
+  def create
+    new_matched_voice_hash = JSON.parse(request.body.read)
+    @new_entry = MatchedVoice.new({
+      book_id: new_matched_voice_hash["book_id"],
+      url: new_matched_voice_hash["url"],
+      talentid: new_matched_voice_hash["talentid"],
+      booking: new_matched_voice_hash["booking"],
+      comment: new_matched_voice_hash["comment"]
+    })
+    if @new_entry.save
+      render json: @new_entry, adapter: :json
+    end
+  end
+
+end
---
+++ react/src/components/MatchForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+class MatchForm extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      book_id: this.props.book_id,
+      url: this.props.data.url,
+      talentid: this.props.data.talentid,
+      booking: this.props.data.booking,
+      comment: ""
+    }
+    this.commentStateChanger = this.commentStateChanger.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.validateTextEntry = this.validateTextEntry.bind(this);
+    this.postMatchedVoice = this.postMatchedVoice.bind(this);
+  }
+
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    if (this.validateTextEntry(this.state.comment)) {
+      let formPayload = {
+        book_id: this.state.book_id,
+        url: this.state.url,
+        talentid: this.state.talentid,
+        booking: this.state.booking,
+        comment: this.state.comment
+      }
+      this.postMatchedVoice(formPayload);
+    } else {
+      this.setState({ comment: "Please enter a comment" })
+    }
+  }
+
+  postMatchedVoice(event) {
+    fetch(`/api/v1/books/${this.state.book_id}/matched_voices`, {
+      method: 'POST',
+      body: JSON.stringify(event)
+    })
+    .then(response => response.json())
+  }
+
+  validateTextEntry(input) {
+    return (input != '' && input != null)
+  }
+
+  commentStateChanger(event) {
+    this.setState({ comment: event.target.value })
+  }
+
+
+  render() {
+    return (
+      <div className="MatchForm">
+        <h3>You're listening to {this.props.data.talentid}</h3>
+        <audio controls>
+          <source src={this.props.data.url} />
+          Your user agent does not support the HTML5 Audio element.
+        </audio>
+        <p>Input below:</p>
+
+        <form onSubmit={this.handleFormSubmit}>
+          <label htmlFor={this.state.comment}>
+          <input
+            name="comment"
+            onChange={this.commentStateChanger}
+            type="text"
+            value={this.state.comment}
+          />
+        </label>
+      </form>
+
+      </div>
+    )
+  }
+}
+
+export default MatchForm;
