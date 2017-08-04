49b24f65e04b0d93b7365761def39f0c3a345b37
Michelle Nosal <nosalcontact@yahoo.com>
2017-08-03 21:33:08 -0400

    end of day

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module Voiceovervoter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
