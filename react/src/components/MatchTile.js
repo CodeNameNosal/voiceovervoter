b795db80a5b4e348eb5868a13b84aa139204873d
Michelle Nosal <nosalcontact@yahoo.com>
2017-08-10 10:31:32 -0400

    compact styling

--- react/src/components/MatchTile.js
+++ react/src/components/MatchTile.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const MatchTile = props => {
   return (
-    <li className="MatchTile">
+    <li className="Tile">
       <p>Narrator: <a href={props.data.booking}>{props.data.talentid}</a></p>
       <p>Demographic: {props.demo}</p>
 
       <audio controls>
-          <source src={props.data.url} />
-          Your user agent does not support the HTML5 Audio element.
+        <source src={props.data.url} />
+        Your user agent does not support the HTML5 Audio element.
       </audio>
       <p>{props.data.comment}</p>
       <p onClick={props.deleteMatch}>Delete this match?</p>
