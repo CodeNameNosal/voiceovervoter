dfe2f356237dbd1df5009782f697ab8870079056
Michelle Nosal <nosalcontact@yahoo.com>
2017-08-10 17:52:35 -0400

    style Matches

--- react/src/components/MatchTile.js
+++ react/src/components/MatchTile.js
@@ -2,17 +2,20 @@ import React from 'react'
 
 const MatchTile = props => {
   return (
-    <li className="Tile row">
-      <div className="small-11 columns">Narrator: <a href={props.data.booking}>{props.data.talentid}</a></div>
-      <div className="small-1 columns"><i onClick={props.deleteMatch} className="fa fa-times fa-lg"></i></div>
-      <p>Demographic: {props.demo}</p>
+    <li>
+      <div className="Tile IndividualMatch row">
+        <div className="small-11 columns">
+        Narrator: <a href={props.data.booking}>{props.data.talentid}</a>
+        </div>
+        <div className="small-1 columns"><i onClick={props.deleteMatch} className="fa fa-times fa-lg"></i></div>
+        <p>Demographic: {props.demo}</p>
 
-      <audio controls>
-        <source src={props.data.url} />
-        Your user agent does not support the HTML5 Audio element.
-      </audio>
-      <p>{props.data.comment}</p>
-
+        <audio controls>
+          <source src={props.data.url} />
+          Your user agent does not support the HTML5 Audio element.
+        </audio>
+        <p>{props.data.comment}</p>
+      </div>
     </li>
   )
 }
