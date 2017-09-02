20edf088743e5f376b71c027cffc3825e47a70a2
Michelle Nosal <nosalcontact@yahoo.com>
2017-09-02 01:28:18 -0400

    custom start and stop

--- react/src/components/MatchTile.js
+++ react/src/components/MatchTile.js
@@ -4,6 +4,7 @@ class MatchTile extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      playStatus: false,
       editMode: false,
       databaseComment: this.props.data.comment,
       newComment: this.props.data.comment
@@ -12,6 +13,7 @@ class MatchTile extends Component {
   this.editCommentFromMatchTile = this.editCommentFromMatchTile.bind(this);
   this.handleFormSubmit = this.handleFormSubmit.bind(this);
   this.validateTextEntry = this.validateTextEntry.bind(this);
+  this.playSound = this.playSound.bind(this);
   }
 
   validateTextEntry(input) {
@@ -41,8 +43,26 @@ class MatchTile extends Component {
     }
   }
 
+  playSound() {
+    const player = document.querySelector(`audio[id="clip${this.props.data.id}"]`);
+    if (this.state.playStatus) {
+      player.pause();
+      this.setState({ playStatus: false })
+    } else {
+      player.currentTime = 0;
+      player.play();
+      this.setState({ playStatus: true })
+      setTimeout(function() { this.setState({ playStatus: false}); }.bind(this), (player.duration*1000));
+    }
+  }
+
   render() {
-    let comment;
+    let icon, comment;
+    if (this.state.playStatus){
+      icon = <i className="fa fa-stop fa-5x" aria-hidden="true"></i>
+    } else {
+      icon = <i className="fa fa-play fa-5x" aria-hidden="true"></i>
+    }
     if (this.state.editMode) {
       comment = <form onSubmit={this.handleFormSubmit}>
         <label htmlFor={this.state.newComment}>
@@ -60,21 +80,31 @@ class MatchTile extends Component {
 
     return (
       <li>
-        <div className="Tile IndividualMatch row">
-          <div className="small-11 columns">
-          <span>Narrator: <a href={this.props.data.booking}>{this.props.data.talentid}</a></span>
+        <div className="Tile IndividualMatch">
+          <div className="row">
+            <div className="small-3 columns centered" id={`clip${this.props.data.id}`} onClick={this.playSound}>
+              {icon}
+            </div>
+            <div className="small-8 columns">
+            <span>Narrator: <a href={this.props.data.booking}>{this.props.data.talentid}</a></span>
+            <p className="demographic">Demographic: {this.props.demo}</p>
+            </div>
+            <div className="small-1 columns">
+              <i onClick={this.props.deleteMatch} className="fa fa-times fa-lg"></i>
+              <i onClick={this.toggleEditMode} className="fa fa-pencil-square-o fa-lg"></i>
+            </div>
           </div>
-          <div className="small-1 columns">
-            <i onClick={this.props.deleteMatch} className="fa fa-times fa-lg"></i>
-            <i onClick={this.toggleEditMode} className="fa fa-pencil-square-o fa-lg"></i>
+          <div className="row">
+            <div className="small-2 columns">
+              <audio id={`clip${this.props.data.id}`}>
+                <source src={this.props.data.url} />
+                Your user agent does not support the HTML5 Audio element.
+              </audio>
+            </div>
+            <div className="small-10 columns">
+              {comment}
+            </div>
           </div>
-          <p className="demographic">Demographic: {this.props.demo}</p>
-
-          <audio controls>
-            <source src={this.props.data.url} />
-            Your user agent does not support the HTML5 Audio element.
-          </audio>
-          {comment}
         </div>
       </li>
     )
